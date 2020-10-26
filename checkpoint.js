5392b9696be51f77550f5b2c4df0e837e6f4c797
Robert-F-Cameron <70616956+Robert-F-Cameron@users.noreply.github.com>
2020-10-26 07:55:23 -0700

    Update checkpoint.js

--- checkpoint.js
+++ checkpoint.js
@@ -9,7 +9,12 @@ pokemons.forEach(pokemon => {
     .then(pokeInfo => pokeInfo.json())
     .then(pokeResult => pokeResult.types.map(resultVal => Object.values(resultVal)[1]))
     .then(typeName => typeName.map(types => Object.values(types)[0]))
-    .then(result => console.log(`${pokemon.charAt(0).toUpperCase()+pokemon.substr(1)}: ${result.join(', ')}`))
+    .then(result =>{ 
+        let text = (`${pokemon.charAt(0).toUpperCase()+pokemon.substr(1)}: ${result.join(', ')}`)
+        console.log(text)
+        fs.appendFileSync('pokemonTypes.txt', text+'\n')
+        })
+    .catch(error=>console.log(`post error: ${error}`))
     })
 
 
